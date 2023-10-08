cee173cd4b47f1e9d19ec700cad939169d20ee20
BroMaster132 <emirmatov@gmail.com>
2023-10-09 01:39:39 +0600

    Update

--- src/components/SpeedDialButton/SpeedDialButton.jsx
+++ src/components/SpeedDialButton/SpeedDialButton.jsx
@@ -1,17 +1,25 @@
 import './SpeedDialButton.css'
 import React, { useState} from 'react'
 import { SpeedDial } from 'primereact/speeddial';
-
-
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { InputNumber } from 'primereact/inputnumber';    
+import { FileUpload } from 'primereact/fileupload';
         
-
 export const SpeedDialButton = ({products}) => {
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible, ] = useState(false);
+  const [display, setDisplay] = useState(false)
+  const [name, setName] = useState(null)
+  const [description, setDescription] = useState(null)
+  const [price, setPrice] = useState(null)
+
 
   const actionItems = [
-    { icon: 'pi pi-shopping-cart', command: () => { updateHelicopter() } },
-    { icon: 'pi pi-cart-plus', command: () => { addHelicopter() } },
-    { icon: 'pi pi-trash', command: () => { removeHelicopter() } }
+    { icon: 'pi pi-shopping-cart', command: () => { setDisplay(true) } },
+    { icon: 'pi pi-cart-plus', command: () => { setDisplay(true) } },
+    { icon: 'pi pi-trash', command: () => { setDisplay(true) } }
   ];
 
   const addHelicopter = (data) => {
@@ -30,14 +38,49 @@ export const SpeedDialButton = ({products}) => {
       return item
     })
   }
+  const onUpload = (e) => {
+    const files = e.target.files;
+    const reader = new FileReader();
+    reader.readAsText(files[0]);
+    reader.onload = (e) => {
+      const data = JSON.parse(e.target.result)
+      updateHelicopter(data.id, data)
+    }
+  }
+  const footerContent = (
+    <div className="p-dialog-footer">
+      <Button style={{marginRight: '20px', fontSize: '15pt', marginBottom: '20px'}} label="Close" icon="pi pi-times" onClick={() => setDisplay(false)} />
+      <Button style={{fontSize: '15pt', marginBottom: '20px', marginRight: '20px',}} label="Save" icon="pi pi-check" onClick={() => setDisplay(false)} />
+    </div>
+  )
 
   return (
     <div>
         <SpeedDial model={actionItems}  visible={visible} onShow={() => setVisible(true)} onHide={() => setVisible(false)} className={'speedButton'} />
+        <Dialog className='modal' header="Create" visible={display} style={{ width: '50vw' }} onHide={() => setDisplay(false)} footer={footerContent}>
+          <div className='modal-content'>
+            <div>
+              <span>Name</span><br />
+              <InputText className='input' value={name} placeholder='Name' onChange={(e) => setName(e.target.value) } />
+            </div>
+            <div>
+              <span>Price</span><br />
+              <InputNumber className='inputNumber' value={price} onValueChange={(e) => setPrice(e.value)} mode="currency" currency="KZT" locale="ru-ru" />            
+            </div>
+            <div>
+              <span>Description</span><br />
+              <InputTextarea className='input' value={description} onChange={(e) => setDescription(e.target.value)} rows={5} cols={80} />
+            </div>
+            <div>
+              <span>Image</span><br />
+              <FileUpload mode="basic" name="demo[]" url="/api/upload" accept="image/*" chooseLabel='Upload' maxFileSize={1000000} onUpload={onUpload} />
+            </div>
 
+
+          </div>
+        </Dialog>
 
     </div>
   )
 }
 
-// direction='down' style={{ left: 'calc(50% - 2rem)', top: 0 }}
